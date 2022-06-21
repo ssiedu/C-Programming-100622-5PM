34fa1d8f6b9922702165fc1320a11e4495863f70
SSI Digital <42675358+ssiedu@users.noreply.github.com>
2022-06-21 19:55:36 +0530

    operator

---
+++ Relational.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+	int  a=10,b=5;
+	//printf("\n Enter the value of a and b : ");
+	//scanf("%d%d",&a,&b);
+	
+	printf("\n Greater than           : %d",a>b);//1
+	printf("\n Less Than              : %d",a<b);//0
+	printf("\n Greater than equal too : %d",a>=b);//1
+	printf("\n Less than equal too    : %d",a<=b);//0
+	printf("\n Equal too              : %d",a==b);//0
+	printf("\n not equal too          : %d",a!=b);//1
+	
+	getch();
+	
+	
+	
+}
---
+++ sizeof.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	printf("\n size of integer  : %d",sizeof(int));
+	printf("\n size of float    : %d",sizeof(float));
+	printf("\n size of char     : %d",sizeof(char));
+	printf("\n size of double   : %d",sizeof(double));
+	printf("\n size of long int : %d",sizeof(long int));
+	getch();
+}
